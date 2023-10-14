c8008dfd5c8bd0dd040b30c3dae8a43e30c8a71c
Andrea Ciardi <developer@andreaciardi.com>
2023-10-14 14:13:24 +0300

    Theme Provider - Done

--- src/providers/theme/Theme.utilities.ts
+++ src/providers/theme/Theme.utilities.ts
@@ -5,6 +5,7 @@ import {
 	ThemeOptions as MuiThemeOptions,
 	ThemeProvider as MuiThemeProvider,
 } from '@mui/material/styles';
+import { PaletteMode as MuiPaletteMode } from '@mui/material';
 import _ from 'lodash';
 
 import {
@@ -45,14 +46,24 @@ const parsePalette = function (
 	return parsedPalette;
 };
 
+const parseBrandParams = function (brand: any) {
+	if (typeof brand?.logo === 'string') {
+		brand.logo = { main: brand?.logo };
+	}
+	return brand;
+};
+
 const parseBrand = function ({ palette, ...brand }: TBrandDefinition): TBrand {
 	let parsedBrandPalette = parsePalette(palette);
+	let parsedBrandParams = parseBrandParams(brand);
 	return _.merge(
 		{},
 		Object.keys(parsedBrandPalette).length > 0
 			? { palette: parsedBrandPalette }
 			: {},
-		brand != null && Object.keys(brand).length > 0 ? { brand: brand } : {}
+		parsedBrandParams != null && Object.keys(parsedBrandParams).length > 0
+			? { brand: parsedBrandParams }
+			: {}
 	);
 };
 
@@ -67,22 +78,24 @@ const parseComponents = function (
 	return parsedComponents;
 };
 
-export const createBrandedTheme = function (
-	brand: TBrandDefinition,
-	baseBrand: TBrandDefinition
-): MuiThemeOptions {
-	const brandedTheme: TBrand = parseBrand(_.merge({}, baseBrand, brand));
-	return brandedTheme;
-};
-
 export const createPaletteTheme = function (
 	palette: TPaletteDefinition,
-	basePalette: TPaletteDefinition
+	basePalette: TPaletteDefinition,
+	mode: MuiPaletteMode
 ): MuiThemeOptions {
 	const paletteTheme: TPalette = parsePalette(
 		_.merge({}, basePalette ?? {}, palette)
 	);
-	return { palette: paletteTheme };
+	return { palette: _.merge({}, paletteTheme, { mode: mode }) };
+};
+
+export const createBrandedTheme = function (
+	brand: TBrandDefinition,
+	baseBrand: TBrandDefinition
+): MuiThemeOptions {
+	// debugger;
+	const brandedTheme: TBrand = parseBrand(_.merge({}, baseBrand, brand));
+	return brandedTheme;
 };
 
 export const createComponentsTheme = function (
@@ -93,4 +106,17 @@ export const createComponentsTheme = function (
 		_.merge({}, baseComponents ?? {}, components)
 	);
 	return { components: componentsTheme };
+};
+
+export const getCustomValue = function (
+	customLight: any,
+	customDark: any,
+	mode: MuiPaletteMode,
+	modeIsToggable: boolean
+) {
+	return modeIsToggable === true
+		? _.merge({}, customLight, mode === 'dark' ? customDark : {})
+		: mode === 'light'
+		? { ...customLight }
+		: { ...customDark };
 };
--- src/providers/themeOLD/index.ts
+++
@@ -1,2 +0,0 @@
-export { ThemeProvider, useTheme } from './Theme';
-export { paletteToVariants } from './Theme.defaults';
